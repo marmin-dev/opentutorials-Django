7d41d068315dc69d3f563b4e90d38c33c1d32b0d
Marmin <marmin.dev@gmail.com>
2023-02-24 06:10:31 +0900

    end of work

---
+++ myapp/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render,HttpResponse,redirect
+import random
+from django.views.decorators.csrf import csrf_exempt
+
+next_id = 4
+topics = [
+{'id':1,'title':'routing','body':'Routing is ...'},
+{'id':2,'title':'view','body':'View is ...'},
+{'id':3,'title':'model','body':'Model is ...'}
+]
+
+def HTMLTemplate(articleTag, id=None):
+    global topics
+    contextUI =''
+    if id != None:
+        contextUI = f'''
+        <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+                <li><a href="/update/{id}/">update</a></li>
+                </li>
+        '''
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+        <html>
+        <body>
+            <h1><a href="/">Django</a></h1>
+            <ul>
+                {ol}
+            </ul>
+            {articleTag}
+            <ul>
+                <li><a href="/create/">create</a></li>
+                {contextUI}
+            <ul>
+            </body>
+        </html>
+        '''
+# Create your views here.
+# 변수화 재사용 가능한 코드로 만들기
+def index(request):
+    article = '''
+        <h2>Welcome</h2>
+            Hello Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+@csrf_exempt
+def create(request):
+    global next_id
+    if request.method == 'GET':
+        article = '''
+        <form action="/create/" method="post">
+            <p><input type="text" name="title" placeholder="title" ></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == "POST":
+        title = request.POST["title"]
+        body = request.POST["body"]
+        newTopic = {"id":next_id,"title":title,"body":body}
+        url = '/read/' + str(next_id)
+        next_id += 1
+        topics.append(newTopic)
+        
+        return redirect(url)
+
+def read(request, id):
+    global topics
+    article = ""
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f"<h2>{topic['title']}</h2> {topic['body']}"
+    return HttpResponse(HTMLTemplate(article,id))
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == "POST":
+        id = request.POST["id"]
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
+
+@csrf_exempt
+def update(request,id):
+    global topics
+    for topic in topics:
+        if topic["id"] == int(id):
+            selectedTopic = {"title":topic['title'],"body":topic['body']}
+    if request.method == "GET":
+        article = f'''
+        <form action="/update/{id}/" method="post">
+            <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]} ></p>
+            <p><textarea name="body" placeholder="body">{selectedTopic["body"]}</textarea></p>
+            <p><input type="submit"></p>
+        </form>
+        '''
+        return HttpResponse(HTMLTemplate(article,id))
+    elif request.method == "POST":
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic["title"] = title
+                topic['body'] = body
+        return redirect(f"/read/{id}")
